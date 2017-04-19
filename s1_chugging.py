dfe6cf96bfa01776c11c01c37ba85745afa47e22
Seth Frey <seth.frey@dartmouth.edu>
2017-04-19 11:17:48 -0400

    added features
    added cases for catching errors
    added feature related to others' game
    added diff-level controsl

--- s1_chugging.py
+++ s1_chugging.py
@@ -20,7 +20,6 @@ import copy
 import csv
 import json
 settings = json.load(open('settings.json', encoding='utf-8'))
-print(settings)
 
 ### for handling deeply nested objects
 ###  https://www.haykranen.nl/2016/02/13/handling-complex-nested-dicts-in-python
@@ -46,6 +45,19 @@ class DQ(DictQuery):
     pass
 
 # fns
+def changeChoicePerspective( sStrat ):
+    if sStrat == "Top":
+        return( "Left" )
+    elif sStrat == "Bottom":
+        return( "Right" )
+    elif sStrat == "Left":
+        return( "Top" )
+    elif sStrat == "Right":
+        return( "Bottom" )
+def changeOutcomePerspective( outcome ):
+    s1, s2 = outcome.split(',')
+    # note reversal of s1 and s2
+    return( changeChoicePerspective( s2 ) + ',' + changeChoicePerspective( s1 ) )
 
 def buildChooseGameIdToQuestionHash(  questionArray ):
     idsToQuestions = {}
@@ -54,9 +66,12 @@ def buildChooseGameIdToQuestionHash(  questionArray ):
         dq = DictQuery(oData)
         # pprint( oData )
         #if oData["theData"]['type'] == 'chooseStrategy':
-        if dq.get("theData/type") == 'chooseStrategy':
-            q = oData["theDataConsummated"] if oData.get("theDataConsummated") else oData["theData"]
-            idsToQuestions[ oData["_id"] ] = q
+        #if dq.get("theData/type") == 'chooseStrategy':
+        if oData.get("theDataConsummated") :
+            q = oData["theDataConsummated"]
+        else :
+            q = oData["theData"]
+        idsToQuestions[ oData["_id"] ] = q
     return( idsToQuestions)
 
 # now let me index on (round, type, asst)
@@ -82,27 +97,75 @@ def buildRndToQuestionHash(  questionArray ):
 def enrichQuestionObject(  q, qdb ):
     # now do some merging from neighboring questions
     asst = q["mtAssignmentId"]
+    sec = q["sec"]
     rnd = q["sec_rnd"]
     qtype = q["type"]
     mtid = q["mtWorkerId"]
-    sec = q["sec"]
     qOId = q["matchingGameId"]
     # for round 1 or 2 question
     #     get preferred choice question from same round 
     #     get expected choice question from same round 
     #     get expected outcome question from previous question
     #     get choice from matching round 4 question (if exists)
-    #     get choice from other player's matching question 
+    if (rnd == 0 or rnd == 1 ) and qtype == "chooseStrategy":
+        # get preferred choice question from same round 
+        qPref = qdb[asst][sec][rnd]["chooseOutcome"]
+        assert qPref["text"][0:13] != "Hypothetically", qPref["text"][0:13]
+        assert qPref["title"] == "Question 1.2" or qPref["title"] == "Question 2.2", qPref
+        q["outcomePreferred"] = qPref["choice"]
+        q["outcomePreferredID"] = qPref["_id"]
+
+        qPred = qdb[asst][sec][rnd]["chooseStrategyTop"]
+        assert qPred["title"] == "Question 1.3" or qPred["title"] == "Question 2.3", qPred
+        q["choicePredicted"] = qPred["choice"]
+        q["choicePredictedID"] = qPred["_id"]
+        q["outcomePredicted"] = q["choice"] + ',' + q['choicePredicted']
+
+        if not qdb[asst][sec].get(4): return( q )
+        qRepeat = qdb[asst][sec][4]["chooseStrategy"]
+        assert qRepeat["title"] == "Question 4" or qRepeat["title"] == "Question", qRepeat
+        q["choiceRepeated"] = qRepeat["choice"]
+        q["choiceRepeatedID"] = qRepeat["_id"]
+    else:
+        print("PROBLEM OFJDFLJG8: why did you even call me?", rnd, qtype, q)
+    return( q )
+
+def enrichQuestionObjectOtherPlayer(  q, iddb, qdb ):
+    # now do some merging from neighboring questions
+    asst = q["mtAssignmentId"]
+    sec = q["sec"]
+    rnd = q["sec_rnd"]
+    qtype = q["type"]
+    mtid = q["mtWorkerId"]
+    qOtherID = q["matchingGameId"]
+    qOther = iddb[qOtherID]
+    asstOther = qOther["mtAssignmentId"]
+    secOther = qOther["sec"]
+    rndOther = qOther["sec_rnd"]
+    qtypeOther = qOther["type"]
+    assert rnd == rndOther, json.dumps(q, indent=1) + json.dumps(qOther, indent=1)
+    assert qOther["matchingGameId"] == q["_id"], json.dumps(q, indent=1) + json.dumps(qOther, indent=1)
+    # for round 1 or 2 question
     #     get preferred choice from other player's matching question 
     #     get expected choice from other player's matching question 
-    if (rnd == 0 or rnd == 1 ) and type == "chooseStrategy":
-        # get preferred choice question from same round 
-        qPref = qdb[asst][rnd]["chooseOutcome"]
-        if qPref["text"][0:14] != "Hypothetically":
-            print( "PROBLEM ARJGF556:", qPref["text"], qPref, q)
-        q["outcomePreferred"] = qPref["choice"]
+    #     get choice from other player's matching question 
+    if (rnd == 0 or rnd == 1 ) and qtype == "chooseStrategy":
+        #qPred = qdb[asst][sec][rnd]["chooseStrategyTop"]
+        #assert qPred["title"] == "Question 1.3" or qPred["title"] == "Question 2.3", qPred
+        #q["choicePredicted"] = qPred["choice"]
+        #q["outcomePredictedID"] = qPred["_id"]
+        qPrefO = qdb[asstOther][secOther][rndOther]["chooseOutcome"]
+        qPredO = qdb[asstOther][secOther][rndOther]["chooseStrategyTop"]
+        q["choiceOther"] = changeChoicePerspective( qOther["choice"] )
+        q["outcomePreferredOther"] = changeOutcomePerspective( qPrefO["choice"] )
+        q["choicePredictedOther"] = changeChoicePerspective( qPredO["choice"] )
+        q["outcomePredictedOther"] = changeOutcomePerspective( qOther["choice"] + ',' + qPredO["choice"] )
+        if not qdb[asstOther][secOther].get(4): return( q )
+        qRepeatO = qdb[asstOther][secOther][4]["chooseStrategy"]
+        q["choiceRepeatedOther"] = changeChoicePerspective( qRepeatO["choice"] )
+        pass
     else:
-        print("PROBLEM OFJDFLJG8: why did you even call me?")
+        print("PROBLEM IOFDL: this is fine except why did you even call me?", rnd, qtype, q)
     return( q )
 
 def gprint( payoffs ):
@@ -131,7 +194,10 @@ def main( sIn, sOut ):
     twoPSpace = OrdinalGameSpace(2)
     nGameCount = 0
     with open(sOut, 'w') as fOut:
-        features = [ "block", "RT", "nPNash", "wwGame", "effGame", "effGameF", "effGameO", "expdGameF", "expdGameO", ]
+        features = [ "gameRT", "nPNash", "nWPNash", "wwGame", "effGameF", "effGameO", "gratFGameF", "gratOGameF", "predGameF", "gratGameO", "predGameO", "cnssGame", "stratCertGame" ]
+        featuresNOT = ["effGame", "habitGameF", "habitGameO", "expdGameF", "expdGameO", "gratGameF", ]
+        ## diff level
+        features.extend(["block","expdGameF","expdGameO","outRT"])
         fCSV = csv.DictWriter( fOut, fieldnames=features )
         fCSV.writeheader()
         for game in gameChoices:
@@ -153,21 +219,46 @@ def main( sIn, sOut ):
                 if not g2Q:
                     print("FAIL question q2", g2Q, q["idGameQ2"] )
                     continue
+                g1Q = enrichQuestionObject(g1Q, rndsToQuestions )
+                g2Q = enrichQuestionObject(g2Q, rndsToQuestions )
+                if not g1Q["matchingGameId"] is None:
+                    g1Q = enrichQuestionObjectOtherPlayer(g1Q, idsToQuestions, rndsToQuestions )
+                if not g2Q["matchingGameId"] is None:
+                    g2Q = enrichQuestionObjectOtherPlayer(g2Q, idsToQuestions, rndsToQuestions )
+                if not all( (f in g1Q and f in g2Q) for f in ["outcome", "choiceRepeated", "choiceRepeatedOther"] ):
+                    print("FAIL q1 or q2 incomplete keys", g1Q["_id"], g2Q["_id"] )
+                    #print("FAIL q1 or q2 incomplete keys", g1Q.keys(), g2Q.keys() )
+                    continue
+
+                ## create game object
                 g1Game = expPayoffToGame( q["payoffsGame1"], twoPSpace )
                 g2Game = expPayoffToGame( q["payoffsGame2"], twoPSpace )
 
                 #pprint(g1Q)
 
+                #print("INtrospect", q['sec'], q['treatment'])
                 # caluc firtst game
                 g1 = {}
-                # controls
-                g1["block"] = 0 if q['sec'] == "experiment1" else 1
-                g1["expdGameF"] = 0 if q['treatment'] == "nofeedback" else 1
-                g1["expdGameO"] = 1 if q['treatment'] == "nofeedback" else 2  # necessarily the opposite in this staggered deisgn
                 # choice rather than game property related
-                g1["RT"] = ( g1Q["choiceMadeTime"] - g1Q["choiceLoadedTime"] ) / 10000 # not g1Q["choiceSubmittedTime"]
-                #
+                g1["gameRT"] = ( g1Q["choiceMadeTime"] - g1Q["choiceLoadedTime"] ) / 10000 # not g1Q["choiceSubmittedTime"]
+                g1["gratGameF"] = 1 if g1Q['outcomePreferred'] == g1Q['outcome'] else 0
+                g1["gratFGameF"] = 1 if g1Q['outcomePreferred'].partition(',')[0] == g1Q['outcome'].partition(',')[0] else 0
+                g1["gratOGameF"] = 1 if g1Q['outcomePreferred'].partition(',')[2] == g1Q['outcome'].partition(',')[2] else 0
+                g1["predGameF"] = 1 if g1Q['outcomePredicted'] == g1Q['outcome'] else 0
+                ## i obviously can't put habitGameF in the model.  talk about selection bias
+                #g1["habitGameF"] = 1 if (g1Q['choice'] == g1Q['choiceRepeated']) else 0
+                #print(g1Q['outcome'], g1Q['choice'], g1Q['outcomePreferred'], g1Q['choicePredicted'], (g1Q['choice'] + ',' + g1Q['choicePredicted']))
+                # other player
+                print( g1Q['outcome'], g1Q['outcomePreferred'], g1Q['outcomePreferredOther'], g1Q['outcomePredicted'], g1Q['outcomePredictedOther'] )
+                g1["gratGameO"] = 1 if g1Q['outcomePreferredOther'] == g1Q['outcome'] else 0
+                g1["predGameO"] = 1 if g1Q['outcomePredictedOther'] == g1Q['outcome'] else 0
+                #g1["habitGameO"] = 1 if (g1Q['choiceOther'] == g1Q['choiceRepeatedOther']) else 0
+                g1["cnssGame"] = 1 if g1Q['outcomePreferred'] == g1Q['outcomePreferredOther'] else 0
+                g1["stratCertGame"] = 1 if g1Q['outcomePredicted'] == g1Q['outcomePredictedOther'] else 0
+
+                # game properties
                 g1["nPNash"] = len( g1Game.findNashEq() )
+                g1["nWPNash"] = len( g1Game.findWeakNashEq() )
                 g1["wwGame"] = 1 if g1Game.isWinWin() else 0
                 g1["effGame"] = g1Game.efficiencyOfGame()
                 g1["effGameF"] = g1Game.efficiencyOfGame(0)
@@ -175,11 +266,19 @@ def main( sIn, sOut ):
 
                 # secodn game
                 g2 = {}
-                g2["block"] = 0 if q['sec'] == "experiment1" else 1
-                g2["expdGameF"] = 0 if q['treatment'] == "nofeedback" else 1
-                g2["expdGameO"] = 1 if q['treatment'] == "nofeedback" else 2
-                g2["RT"] = ( g2Q["choiceMadeTime"] - g2Q["choiceLoadedTime"] ) / 10000
+                g2["gameRT"] = ( g2Q["choiceMadeTime"] - g2Q["choiceLoadedTime"] ) / 10000
+                g2["gratGameF"] = 1 if g2Q['outcomePreferred'] == g2Q['outcome'] else 0
+                g2["gratFGameF"] = 1 if g2Q['outcomePreferred'].partition(',')[0] == g2Q['outcome'].partition(',')[0] else 0
+                g2["gratOGameF"] = 1 if g2Q['outcomePreferred'].partition(',')[2] == g2Q['outcome'].partition(',')[2] else 0
+                g2["predGameF"] = 1 if (g2Q['choice'] + ',' + g2Q['choicePredicted']) == g2Q['outcome'] else 0
+                #g2["habitGameF"] = 1 if (g2Q['choice'] == g2Q['choiceRepeated']) else 0
+                g2["gratGameO"] = 1 if g2Q['outcomePreferredOther'] == g2Q['outcome'] else 0
+                g2["predGameO"] = 1 if g2Q['outcomePredictedOther'] == g2Q['outcome'] else 0
+                #g2["habitGameO"] = 1 if (g2Q['choiceOther'] == g2Q['choiceRepeatedOther']) else 0
+                g2["cnssGame"] = 1 if g2Q['outcomePreferred'] == g2Q['outcomePreferredOther'] else 0
+                g2["stratCertGame"] = 1 if g2Q['outcomePredicted'] == g2Q['outcomePredictedOther'] else 0
                 g2["nPNash"] = len( g2Game.findNashEq() )
+                g2["nWPNash"] = len( g2Game.findWeakNashEq() )
                 g2["wwGame"] = 1 if g2Game.isWinWin() else 0
                 g2["effGame"] = g2Game.efficiencyOfGame()
                 g2["effGameF"] = g2Game.efficiencyOfGame(0)
@@ -196,8 +295,14 @@ def main( sIn, sOut ):
                 # get diff
                 diffGame = {}
                 for f in features:
-                    diffGame[f] = chosenGame[f] - otherGame[f]
+                    if f in chosenGame:
+                        diffGame[f] = chosenGame[f] - otherGame[f]
 
+                # diff-level controls
+                diffGame["block"] = 0 if q['sec'] == "experiment1" else 1
+                diffGame["expdGameF"] = 0 if q['treatment'] == "nofeedback" else 1
+                diffGame["expdGameO"] = 1 if q['treatment'] == "nofeedback" else 0  # necessarily the opposite in this staggered deisgn
+                diffGame["outRT"] = ( g2Q["choiceMadeTime"] - g2Q["choiceLoadedTime"] ) / 10000
                 ### output
                 ### csvwriter
                 #print(features)
@@ -223,11 +328,12 @@ class TestOrdinalGame(unittest.TestCase):
         #self.twoSpace = OrdinalGameSpace(2)
         #self.pd = EmpiricalOrdinalGame2P( np.array([[[3,3],[1,4]],[[4,1],[2,2]]] ))
         #self.sh = EmpiricalOrdinalGame2P( np.array([[[3,3],[1,2]],[[2,1],[4,4]]] ))
-        self.gameChoices = json.load(open(
+        with open(
             settings["dataTest"] + 'SubDataTestSample.json',
             'r',
             encoding='utf-8'
-        ))
+        ) as testFile:
+            self.gameChoices = json.load(testFile)
 
     def test_buildChooseGameIdToQuestionHash(self):
         qHash = buildChooseGameIdToQuestionHash( self.gameChoices )
@@ -255,10 +361,27 @@ class TestOrdinalGame(unittest.TestCase):
         #np.testing.assert_array_equal( self.pd.outcomes[ self.pd.foundNashEq[0] ], np.array([2,2]) )
 
     def test_enrichQuestionObject( self ):
-        pass
+        qIdHash = buildChooseGameIdToQuestionHash( self.gameChoices )
+        qRndHash = buildRndToQuestionHash( self.gameChoices )
+        q1 = qIdHash["aDsp4bhaLxWM9NDnt"]
+        qnew = enrichQuestionObject( q1, qRndHash)
+        self.assertTrue( qnew.get( "outcomePreferredID", qnew ) )
+        self.assertEqual( qnew["outcomePreferredID"], "dsKwc27cCJyyLovyd"  )
+        self.assertEqual( qnew["outcomePreferred"], qIdHash[ qnew["outcomePreferredID"] ]["choice"]  )
+        self.assertTrue( qnew.get( "outcomePredictedID", qnew ) )
+        self.assertEqual( qnew["choicePredictedID"], "4PvmaGuw4gq4BBikj" )
+        self.assertEqual( qnew["choicePredicted"], qIdHash[ qnew["choicePredictedID"] ]["choice"]  )
+        self.assertTrue( qnew.get( "choiceRepeatedID", qnew ) )
+        self.assertEqual( qnew["choiceRepeatedID"], "Nhiu2HeQKuxKBHKBu" )
+        self.assertEqual( qnew["choiceRepeated"], qIdHash[ qnew["choiceRepeatedID"] ]["choice"]  )
 
-test = True
-if test :
+    def test_changePerspective( self ):
+        self.assertEqual( "Top,Left" , changeOutcomePerspective( "Top,Left" ))
+        self.assertEqual( "Bottom,Left" , changeOutcomePerspective( "Top,Right" ))
+
+
+testing = False
+if testing :
     print("testing")
     #gameChoices = json.load(open(
         #settings["dataTest"] + 'SubDataTestSample.json',
